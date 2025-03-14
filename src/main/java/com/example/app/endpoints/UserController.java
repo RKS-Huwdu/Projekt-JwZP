568a6dcd4ea08147e20a4f78c3dff32a93ea0233
Karol Bargiel <karibargiel@gmail.com>
2025-03-14 16:46:04 +0100

    added config package and swagger infos

--- src/main/java/com/example/app/endpoints/UserController.java
+++ src/main/java/com/example/app/endpoints/UserController.java
@@ -2,6 +2,9 @@ package com.example.app.endpoints;
 
 import com.example.app.entities.User;
 import com.example.app.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User", description = "User management")
 public class UserController {
 
     private final UserService userService;
@@ -18,11 +22,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Get all users",
+            description = "Get all users from the database",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK"
+                    )
+            }
+    )
     @GetMapping
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
+    @Operation(
+            summary = "Get user by ID",
+            description = "Get user from the database by ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK"
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return userService.findById(id)
@@ -30,12 +54,32 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(
+            summary = "Create user",
+            description = "Create user in the database",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "User created"
+                    )
+        }
+    )
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
+    @Operation(
+            summary = "Delete user",
+            description = "Delete user from the database by ID",
+    responses = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "OK"
+        )
+    }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         return userService.deleteById(id) ?
