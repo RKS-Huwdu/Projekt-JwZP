c5359937777e88f66637c06d3250cf533bac5480
Sebastian Iciek <sebastianiciek04@gmail.com>
2025-04-02 19:21:56 +0200

    fix to global exception handler

--- src/main/java/com/example/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/app/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameNotFoundException.class)
